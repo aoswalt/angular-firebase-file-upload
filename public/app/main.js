abce02c9b339c2a5b0c2ceee5feba6a4e7d5e8f7
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-10 10:05:53 -0500

    file retrieval

--- public/app/main.js
+++ public/app/main.js
@@ -1,5 +1,20 @@
 angular.module("app", [])
+  .config(() => {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDnwDqZiVYoz8OS1t8yYikfjEEfQAYbNZA",
+      authDomain: "project-5923074717115769440.firebaseapp.com",
+      databaseURL: "https://project-5923074717115769440.firebaseio.com",
+      storageBucket: "project-5923074717115769440.appspot.com"
+    });
+  })
+
   .controller("UploadCtrl", function() {
     const up = this;
     up.heading = "Up the photos!";
+
+    up.submit = () => {
+      const input = document.querySelector(`[type="file"]`);
+      const file = input.files[0];
+      console.dir(file);
+    };
   });
